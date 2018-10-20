44f1433eb2be0a1c511ef7e6946d503c34d31981
nllsfx <nllsdfx@gmail.com>
2018-10-20 23:29:56 +0300

    first commit

---
+++ Junker.lua
@@ -0,0 +1,42 @@
+local function JunkerReapairAllItems()
+
+    if not (CanMerchantRepair()) then
+        return false;
+    end
+
+    if (IsInGuild(UnitName("player")) and CanGuildBankRepair()) then
+        RepairAllItems(1);
+    else
+        RepairAllItems();
+    end
+end
+
+local function JunkerSellGrayItems()
+    local i = 0;
+    repeat
+        local slots = GetContainerNumSlots(i)
+        if not (slots == nil) then
+            local j = 1;
+            repeat
+                local link = GetContainerItemLink(i, j);
+                if not (link == nil) then
+                    local _, _, quality = GetItemInfo(link);
+                    if (quality == 0) then
+                        UseContainerItem(i, j);
+                    end
+                end
+                j = j + 1;
+            until (j >= slots + 1)
+        end
+        i = i + 1;
+    until (i >= 5)
+end
+
+function JunkerInit()
+    frmjunker:RegisterEvent("MERCHANT_SHOW");
+end
+
+function JunkerEvent()
+    JunkerReapairAllItems();
+    JunkerSellGrayItems();
+end
